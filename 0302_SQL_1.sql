6ab22b12c0e8858a21ffdfd2e2c8de1029786504
許嘉哲 <161096703+Rogerhsu12345@users.noreply.github.com>
2024-03-02 11:46:33 +0800

    Create 0302_SQL_1.sql

---
+++ 0302_SQL_1.sql
@@ -0,0 +1,24 @@
+CREATE TABLE IF NOT EXISTS student(
+	student_id SERIAL PRIMARY KEY,
+	name VARCHAR(20),
+	major VARCHAR(20) 
+);
+
+DROP TABLE student;
+
+CREATE TABLE IF NOT EXISTS artists(
+    id SERIAL PRIMARY KEY,
+	name VARCHAR(200)	
+);
+
+DROP TABLE artists;
+
+SELECT *
+FROM artists;
+
+
+
+
+
+
+
