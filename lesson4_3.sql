5bdbf7d01723b3b21b1a0ae97eec909be7d116e6
許嘉哲 <161096703+Rogerhsu12345@users.noreply.github.com>
2024-03-02 16:05:15 +0800

    Update lesson4_3.sql

--- lesson4_3.sql
+++ lesson4_3.sql
@@ -14,13 +14,76 @@ INSERT INTO student VALUES(4,'小藍','英語',80);
 INSERT INTO student VALUES(5,'小黑','化學',20);
 
 SELECT *
-FROM student
+FROM student;
 
 SELECT name
-FROM student
+FROM student;
 
 SELECT name,major
-FROM student
+FROM student;
 
 SELECT name AS 姓名,major AS 主修
+FROM student;
+
+SELECT *
 FROM student
+ORDER BY score ASC;
+
+SELECT *
+FROM student
+ORDER BY score DESC;
+
+SELECT *
+FROM student
+ORDER BY score DESC
+LIMIT 3;
+
+SELECT *
+FROM student
+ORDER BY score
+LIMIT 2;
+
+
+SELECT *
+FROM student
+WHERE major = '英語'
+ORDER BY score DESC
+LIMIT 1;
+
+SELECT *
+FROM student
+WHERE major = '英語' AND score < 60;
+
+SELECT *
+FROM student
+WHERE major = '英語' OR score > 60;
+
+SELECT *
+FROM student
+WHERE major = '英語' OR major ='生物' OR major ='歷史';
+
+SELECT *
+FROM student
+WHERE major IN('英語','生物','歷史');
+
+SELECT *
+FROM student
+WHERE score >= 80 AND score <= 100;
+
+SELECT *
+FROM student
+WHERE score BETWEEN 80 AND 100;
+
+SELECT *
+FROM student
+WHERE NOT name ='小黃';
+
+/*第一個字為小*/
+SELECT *
+FROM student
+WHERE name LIKE '小%';
+
+SELECT *
+FROM student
+WHERE name LIKE '%黃%';
+
