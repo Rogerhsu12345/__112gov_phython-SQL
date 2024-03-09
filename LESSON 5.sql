8a923abf35b6294092331b66ca1b61777bd5474b
許嘉哲 <161096703+Rogerhsu12345@users.noreply.github.com>
2024-03-09 10:28:01 +0800

    Update LESSON 5.sql

--- LESSON 5.sql
+++ LESSON 5.sql
@@ -18,3 +18,23 @@ CREATE TABLE IF NOT EXISTS 聯絡人(
 
 DROP TABLE IF EXISTS 聯絡人
 DROP TABLE IF EXISTS 客戶
+
+INSERT INTO 客戶(客戶名稱)
+VALUES('遠傳電信'),
+      ('台灣大車隊')
+
+INSERT INTO 聯絡人(客戶id,聯絡人姓名,電話,郵件)
+VALUES (1,'JOHN Doe','0972185666','john.doe@bluebird.dev'),
+      (1,'JOHN Doe','0972185666','jaaahn.doe@bluebird.dev'),
+      (2,'JOHN Doe','0972185666','john.doe@bluebird.dev');
+
+DELETE FROM 客戶
+WHERE 客戶_id = 1
+
+
+
+
+
+
+
+
