1765975e9189c154c6d3ebcf10345d082f9c2985
許嘉哲 <161096703+Rogerhsu12345@users.noreply.github.com>
2024-03-09 14:55:52 +0800

    Update 火車進出站人數sql.sql

--- 火車進出站人數sql.sql
+++ 火車進出站人數sql.sql
@@ -49,7 +49,17 @@ SELECT DISTINCT 名稱
 FROM gate_count LEFT JOIN stations ON 站點編號=編號
 WHERE 地址 LIKE '基隆市%';
 
-/*基隆火車站2022年3月1日資料*/
+/*基隆火車站2022年3月1日資料,'2022-03-01'*/
+SELECT 日期,進站人數,出站人數,名稱,地名,地址,YOUBIKE
+FROM gate_count LEFT JOIN stations ON 站點編號=編號
+WHERE 名稱 = '基隆' AND 日期 = '2022-03-01';
+
+/*取出基隆火車站 2022-03月份資料 時間由小到大排序*/
+SELECT 日期,進站人數,出站人數,名稱,地名,地址,YOUBIKE
+FROM gate_count LEFT JOIN stations ON 站點編號=編號
+WHERE 名稱 = '基隆' AND (日期 BETWEEN '2022-03-01' AND '2022-03-31')
+ORDER BY 日期 ASC;
+
 
 
 
