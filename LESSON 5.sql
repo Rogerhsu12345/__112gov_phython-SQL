18d6be24f8e806d9b4462992fc4ca64867cd3faf
許嘉哲 <161096703+Rogerhsu12345@users.noreply.github.com>
2024-03-09 10:42:02 +0800

    Update LESSON 5.sql

--- LESSON 5.sql
+++ LESSON 5.sql
@@ -14,14 +14,15 @@ CREATE TABLE IF NOT EXISTS 聯絡人(
 	郵件 VARCHAR(100),
 	PRIMARY KEY(聯絡人id),
 	FOREIGN KEY(客戶id) REFERENCES 客戶(客戶_id)	
+	ON DELETE SET NULL 
 	);
 
-DROP TABLE IF EXISTS 聯絡人
-DROP TABLE IF EXISTS 客戶
+DROP TABLE IF EXISTS 聯絡人;
+DROP TABLE IF EXISTS 客戶;
 
 INSERT INTO 客戶(客戶名稱)
 VALUES('遠傳電信'),
-      ('台灣大車隊')
+      ('台灣大車隊');
 
 INSERT INTO 聯絡人(客戶id,聯絡人姓名,電話,郵件)
 VALUES (1,'JOHN Doe','0972185666','john.doe@bluebird.dev'),
@@ -30,6 +31,9 @@ VALUES (1,'JOHN Doe','0972185666','john.doe@bluebird.dev'),
 
 DELETE FROM 客戶
 WHERE 客戶_id = 1
+
+SELECT * FROM 客戶;
+SELECT * FROM 聯絡人;
 
 
 
